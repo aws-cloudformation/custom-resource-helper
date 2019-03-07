37bd2fd70f1da665a77e8af7607925d4979437ae
Jay McConnell <jmmccon@amazon.com>
2019-03-07 15:34:26 -0800

    more explicit python version support

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ setup(
     tests_require=["boto3"],
     test_suite="tests",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
