e5ab2d97052cea07901fa556f631f3b1e0f461fe
gruebel <anton.gruebel@gmail.com>
2021-02-08 22:18:48 +0100

    Remove tests folder from package

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     author="Jay McConnell",
     author_email="jmmccon@amazon.com",
     license="Apache2",
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests", "tests.*",)),
     install_requires=[],
     tests_require=["boto3"],
     test_suite="tests",
