a4483363f89b5f8f5edef09459607f6411990179
Jay McConnell <jmmccon@amazon.com>
2021-03-18 13:48:54 -0700

    add tests for ssl_verify options

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,8 @@ import json
 from unittest.mock import patch, Mock, ANY
 from crhelper import utils
 import unittest
+import ssl
+import tempfile
 
 
 class TestLogHelper(unittest.TestCase):
@@ -30,3 +32,32 @@ class TestLogHelper(unittest.TestCase):
             method="PUT",
             url="/this/is/the/url?query=123#aaa",
         )
+
+    @patch('crhelper.utils.ssl.create_default_context', autospec=True)
+    @patch('crhelper.utils.HTTPSConnection', autospec=True)
+    def test_send_response_no_ssl_verify(self, https_connection_mock, ssl_create_context_mock):
+        ctx_mock = Mock()
+        ssl_create_context_mock.return_value = ctx_mock
+        utils._send_response(self.TEST_URL, {}, ssl_verify=False)
+        https_connection_mock.assert_called_once_with("test_url", context=ctx_mock)
+        self.assertFalse(ctx_mock.check_hostname)
+        self.assertEqual(ctx_mock.verify_mode, ssl.CERT_NONE)
+
+    @patch('crhelper.utils.ssl.create_default_context', autospec=True)
+    @patch('crhelper.utils.HTTPSConnection', autospec=True)
+    def test_send_response_custom_ca(self, https_connection_mock, ssl_create_context_mock):
+        ctx_mock = Mock()
+        ssl_create_context_mock.return_value = ctx_mock
+        with tempfile.NamedTemporaryFile() as tmp:
+            utils._send_response(self.TEST_URL, {}, ssl_verify=tmp.name)
+        https_connection_mock.assert_called_once_with("test_url", context=ctx_mock)
+        ctx_mock.load_verify_locations.assert_called_once_with(cafile=tmp.name)
+
+    @patch('crhelper.utils.ssl.create_default_context', autospec=True)
+    @patch('crhelper.utils.HTTPSConnection', autospec=True)
+    def test_send_response_non_existant_custom_ca(self, https_connection_mock, ssl_create_context_mock):
+        ctx_mock = Mock()
+        ssl_create_context_mock.return_value = ctx_mock
+        utils._send_response(self.TEST_URL, {}, ssl_verify='/invalid/path/to/ca')
+        https_connection_mock.assert_called_once_with("test_url", context=ANY)
+        ctx_mock.load_verify_locations.assert_not_called()
